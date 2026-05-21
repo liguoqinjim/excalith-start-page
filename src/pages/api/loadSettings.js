9a7d2f54eb851fe90dd1cb8b3688bf99f78267e9
liguoqinjim <liguoqinjim23@gmail.com>
2026-05-21 10:52:51 +0800

    feat($): support jsonc

--- src/pages/api/loadSettings.js
+++ src/pages/api/loadSettings.js
@@ -1,8 +1,13 @@
 import fs from "fs"
 import path from "path"
+import JSON5 from "json5"
 
 export default function handler(req, res) {
-	const filePath = path.join(process.cwd(), "data", "settings.json")
+	const dataDir = path.join(process.cwd(), "data")
+	const jsoncPath = path.join(dataDir, "settings.jsonc")
+	const jsonPath = path.join(dataDir, "settings.json")
+
+	const filePath = fs.existsSync(jsoncPath) ? jsoncPath : jsonPath
 	const fileContents = fs.readFileSync(filePath, "utf8")
-	res.status(200).json(JSON.parse(fileContents))
+	res.status(200).json(JSON5.parse(fileContents))
 }
--- src/pages/api/saveSettings.js
+++ src/pages/api/saveSettings.js
@@ -2,7 +2,11 @@ import fs from "fs"
 import path from "path"
 
 export default function handler(req, res) {
-	const filePath = path.join(process.cwd(), "data", "settings.json")
+	const dataDir = path.join(process.cwd(), "data")
+	const jsoncPath = path.join(dataDir, "settings.jsonc")
+	const jsonPath = path.join(dataDir, "settings.json")
+
+	const filePath = fs.existsSync(jsoncPath) ? jsoncPath : jsonPath
 	fs.writeFileSync(filePath, JSON.stringify(req.body, null, 2))
 	res.status(200).json({ message: "Settings saved" })
 }
